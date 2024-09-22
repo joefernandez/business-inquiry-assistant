9a8b7c1355f6a0d110a49f3cd42beb80df0e8a55
Joe Fernandez <joefernandez@google.com>
2024-09-22 22:22:17 +0000

    Renamed email-processing-webapp directory for consistency

--- model-tuning/deploy_weights.sh
+++ model-tuning/deploy_weights.sh
@@ -4,7 +4,7 @@
 date_time=$(date +"%Y_%m_%d_%H%M%S")
 
 # backup previous weights
-mv ../email_processing_webapp/weights/gemma2-2b_inquiry_tuned.lora.h5 ../email_processing_webapp/weights/gemma2-2b_inquiry_tuned.lora.h5.$date_time.backup
+mv ../email-processing-webapp/weights/gemma2-2b_inquiry_tuned.lora.h5 ../email-processing-webapp/weights/gemma2-2b_inquiry_tuned.lora.h5.$date_time.backup
 
 # deploy new weights
-cp weights/gemma2-2b_inquiry_tuned_4_epoch3.lora.h5 ../email_processing_webapp/weights/gemma2-2b_inquiry_tuned.lora.h5
+cp weights/gemma2-2b_inquiry_tuned_4_epoch3.lora.h5 ../email-processing-webapp/weights/gemma2-2b_inquiry_tuned.lora.h5
